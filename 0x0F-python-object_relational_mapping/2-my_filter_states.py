e8c78c49a8f7959189bd2d5e43f18805dd5c13dd
dannydms <shadaniels1997@gmail.com>
2023-06-16 00:49:46 -0700

    TaskFrom city

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-""" Script  that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
 """
+Lists all values in the states tables of a database where name
+matches the argument
+"""
+import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    import sys
-    import MySQLdb
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    serv = MySQLdb.connect(host="localhost",  port=3306,
-                           user=sys.argv[1], password=sys.argv[2],
-                           database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-    c = serv.cursor()
-    stateName = sys.argv[4]
-    c.execute(
-        "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(
-            stateName))
-    rows = c.fetchall()
-    for row in rows:
-        if row[1] == stateName:
-            print(row)
-    c.close()
-    serv.close()`:wq
-
+    for state in states:
+        print(state)
